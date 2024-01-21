44b4533c6d10627ab0303a47d4fe62dc34ef3352
Dura G <dureguye2@gmail.com>
2024-01-21 16:07:39 +0300

    Time: 500 ms (95.66%) | Memory: 31.1 MB (56.83%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -1,31 +1,38 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        N =len(nums)
-        maxSubArray = float('-inf')
-        current_sum = 0
-        for num in nums:
-            current_sum = max(num, current_sum+num)
-            maxSubArray = max(maxSubArray,current_sum)
-        return maxSubArray
-    """  
-    brute force approch  
-        N =len(nums)
-        maxSubArray = float('-inf')
-        for i in range(N):
-            current_sum = 0
-            for j in range(i,N):
-                current_sum +=nums[j]
-                maxSubArray = max(maxSubArray,current_sum)
-        return maxSubArray
-        """
-        
-        
-
-
+        maxsum = float('-inf')
+        cursum,L=0, 0
+        for R in range(len(nums)):
+            if cursum < 0:
+                cursum =0
+                L=R
+            cursum +=nums[R]
+            if cursum > maxsum:
+                maxsum =cursum
+        return maxsum
+    #optimal soultion using kadane's algo
+        """max_sum = nums[0]
+        current_sum = nums[0]
+        for i in range(1,len(nums)):
+            current_sum = max(nums[i],current_sum + nums[i])
+            max_sum = max(max_sum, current_sum)
+        return max_sum"""
+    #using sliding windows 
 
     
 
 
+
+    """#brute force approche
+        max_subarray = float('-inf')
+        for i in range(len(nums)):
+            current_sum = 0
+            for j in range(i,len(nums)):
+                current_sum +=nums[j]
+                max_subarray = max(max_subarray,current_sum)
+    """
+        
         
 
-        
+
+      
