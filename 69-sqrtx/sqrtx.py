f644448d5060169fa756d9efdf28e1636d3b4296
Dura G <dureguye2@gmail.com>
2024-01-28 10:32:31 +0300

    Time: 37 ms (81.73%) | Memory: 16.7 MB (54.59%) - LeetSync

---
+++ 69-sqrtx/sqrtx.py
@@ -0,0 +1,22 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x is 1 or 0:
+            return x
+        res = 0
+        left = 1
+        right = x
+        while left <= right:
+            mid = left + (right - left )//2
+            if mid * mid > x:
+                right = mid-1
+           
+            else:
+                left = mid +1   
+
+        return right
+       
+            
+         
+
+        
+        
