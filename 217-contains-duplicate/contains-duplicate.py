31ee4af42209de89c0ed77a0af83c6ad1eccdea0
Dura G <dureguye2@gmail.com>
2024-02-04 15:12:49 +0300

    Time: 413 ms (89.72%) | Memory: 32 MB (61.02%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.py
@@ -0,0 +1,11 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+         return len(set(nums)) < len(nums)
+
+        
+            
+            
+            
+               
+        
+        
